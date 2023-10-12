6d0b8816590d43632d6dca97aa45e1ccb01ea462
Aditya Bhattad <adityabhattad18@gmail.com>
2023-10-13 00:21:44 +0530

    chore: add constants for smart-contract-wallet

--- super-wallet/packages/nextjs/generated/deployedContracts.ts
+++ super-wallet/packages/nextjs/generated/deployedContracts.ts
@@ -84,6 +84,91 @@ const contracts = {
       },
     },
   ],
+  80001: [
+    {
+      chainId: "80001",
+      name: "polygonMumbai",
+      contracts: {
+        WalletFactory: {
+          address: "0x588fB804d43e5DABeAbABd4805acc1a4d4418331",
+          abi: [
+            {
+              inputs: [
+                {
+                  internalType: "contract IEntryPoint",
+                  name: "entryPoint",
+                  type: "address",
+                },
+              ],
+              stateMutability: "nonpayable",
+              type: "constructor",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address[]",
+                  name: "owners",
+                  type: "address[]",
+                },
+                {
+                  internalType: "uint256",
+                  name: "salt",
+                  type: "uint256",
+                },
+              ],
+              name: "createNewSmartContractWallet",
+              outputs: [
+                {
+                  internalType: "contract Wallet",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address[]",
+                  name: "owners",
+                  type: "address[]",
+                },
+                {
+                  internalType: "uint256",
+                  name: "salt",
+                  type: "uint256",
+                },
+              ],
+              name: "getAddress",
+              outputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "walletImplementation",
+              outputs: [
+                {
+                  internalType: "contract Wallet",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+          ],
+        },
+      },
+    },
+  ],
 } as const;
 
 export default contracts;
---
+++ super-wallet/packages/nextjs/utils/getContracts.ts
@@ -0,0 +1,22 @@
+import {Contract,providers} from "ethers";
+import { Constants } from "userop";
+import { ENTRY_POINT_ABI, WALLET_ABI, WALLET_FACTORY_ABI, WALLET_FACTORY_ADDRESS } from "./constants";
+
+export const provider = new providers.JsonRpcProvider(process.env.NEXT_PUBLIC_STACKUP_API_ENDPOINT);
+
+
+export const entryPointContract = new Contract(
+    Constants.ERC4337.EntryPoint,
+    ENTRY_POINT_ABI,
+    provider
+);
+
+export const walletFactoryContract = new Contract(
+    WALLET_FACTORY_ADDRESS,
+    WALLET_FACTORY_ABI,
+    provider
+)
+
+export const getWalletContract = (walletAddress:string) => {
+    return new Contract(walletAddress,WALLET_ABI,provider);
+}
