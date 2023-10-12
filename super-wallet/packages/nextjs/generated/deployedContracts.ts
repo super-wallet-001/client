2b69a1592174665c16d5ce063f86767a53782590
Aditya Bhattad <adityabhattad18@gmail.com>
2023-10-12 22:14:59 +0530

    feat: add create scw page

--- super-wallet/packages/nextjs/generated/deployedContracts.ts
+++ super-wallet/packages/nextjs/generated/deployedContracts.ts
@@ -1,3 +1,89 @@
-const deployedContracts = null;
+const contracts = {
+  31337: [
+    {
+      chainId: "31337",
+      name: "localhost",
+      contracts: {
+        WalletFactory: {
+          address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+          abi: [
+            {
+              inputs: [
+                {
+                  internalType: "contract IEntryPoint",
+                  name: "entryPoint",
+                  type: "address",
+                },
+              ],
+              stateMutability: "nonpayable",
+              type: "constructor",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address[]",
+                  name: "owners",
+                  type: "address[]",
+                },
+                {
+                  internalType: "uint256",
+                  name: "salt",
+                  type: "uint256",
+                },
+              ],
+              name: "createNewSmartContractWallet",
+              outputs: [
+                {
+                  internalType: "contract Wallet",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address[]",
+                  name: "owners",
+                  type: "address[]",
+                },
+                {
+                  internalType: "uint256",
+                  name: "salt",
+                  type: "uint256",
+                },
+              ],
+              name: "getAddress",
+              outputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "walletImplementation",
+              outputs: [
+                {
+                  internalType: "contract Wallet",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+          ],
+        },
+      },
+    },
+  ],
+} as const;
 
-export default deployedContracts;
+export default contracts;
---
+++ super-wallet/packages/nextjs/pages/create-scw.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const CreateSmartContractWalletUI:NextPage = () =>{
+    return (
+        <div>
+            Create Smart Contract Wallet Page.
+        </div>
+    )
+} 
+
+export default CreateSmartContractWalletUI;
