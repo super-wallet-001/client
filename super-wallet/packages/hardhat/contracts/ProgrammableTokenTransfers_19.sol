41aeaaf563450ef6c920c147c1925ea5fbd635f1
sergeyg <sergey.gerodes@raysono.com>
2023-10-18 02:18:05 +0200

    fix string address conversion

--- super-wallet/packages/hardhat/contracts/ProgrammableTokenTransfers_19.sol
+++ super-wallet/packages/hardhat/contracts/ProgrammableTokenTransfers_19.sol
@@ -367,7 +367,7 @@ contract ProgrammableTokenTransfers is CCIPReceiver, OwnerIsCreator {
         lastReceivedMessageId = any2EvmMessage.messageId; // fetch the messageId
         lastReceivedText = abi.decode(any2EvmMessage.data, (string)); // abi-decoding of the sent text
 
-        address recipient = address(bytes20(bytes(lastReceivedText)));
+        address recipient = extractAddress(lastReceivedText);
 
         // Expect one token to be transferred at once, but you can transfer several tokens.
         lastReceivedTokenAddress = any2EvmMessage.destTokenAmounts[0].token;
@@ -383,6 +383,46 @@ contract ProgrammableTokenTransfers is CCIPReceiver, OwnerIsCreator {
             any2EvmMessage.destTokenAmounts[0].token,
             any2EvmMessage.destTokenAmounts[0].amount
         );
+    }
+
+    function fromHexChar(uint8 c) public pure returns (uint8) {
+        if (bytes1(c) >= bytes1('0') && bytes1(c) <= bytes1('9')) {
+            return c - uint8(bytes1('0'));
+        }
+        if (bytes1(c) >= bytes1('a') && bytes1(c) <= bytes1('f')) {
+            return 10 + c - uint8(bytes1('a'));
+        }
+        if (bytes1(c) >= bytes1('A') && bytes1(c) <= bytes1('F')) {
+            return 10 + c - uint8(bytes1('A'));
+        }
+        return 0;
+    }
+
+
+    function hexStringToAddress(string memory s) public pure returns (bytes memory) {
+        bytes memory ss = bytes(s);
+        require(ss.length%2 == 0); // length must be even
+        bytes memory r = new bytes(ss.length/2);
+        for (uint i=0; i<ss.length/2; ++i) {
+            r[i] = bytes1(fromHexChar(uint8(ss[2*i])) * 16 +
+            fromHexChar(uint8(ss[2*i+1])));
+        }
+
+        return r;
+
+    }
+
+
+    function extractAddress(string memory s) public pure returns (address) {
+        bytes memory _bytes = hexStringToAddress(s);
+        require(_bytes.length >= 1 + 20, "toAddress_outOfBounds");
+        address tempAddress;
+
+        assembly {
+            tempAddress := div(mload(add(add(_bytes, 0x20), 1)), 0x1000000000000000000000000)
+        }
+
+        return tempAddress;
     }
 
     /// @notice Construct a CCIP message.
